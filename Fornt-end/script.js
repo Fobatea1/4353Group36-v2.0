1c0d286e7efe7f41d1aeb77818cd58cffa2aae66
Jimmy Vuong <47999366+Fobatea1@users.noreply.github.com>
2024-02-19 04:58:19 +0000

    Can register and login to local storage now. Need to implement register criteria -j

--- Fornt-end/script.js
+++ Fornt-end/script.js
@@ -9,3 +9,35 @@ loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active');});
 
 btnLoginPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup');});
 btnCloseLogin.addEventListener('click', ()=> {wrapper.classList.remove('active-popup');});
+
+
+//check if button works
+
+
+function loginUser(){
+
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    if (username === storedUsername && password === storedPassword) {
+        alert('Login Successful!');
+        wrapper.classList.remove('active-popup'); //closes the form
+        // Redirect or display login success message here
+    } else {
+        alert('Invalid Username or Password.');
+    }
+
+    
+}
+
+function registerUser(){
+
+    const username = document.getElementById('registerUsername').value;
+    const password = document.getElementById('registerPassword').value;
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    alert('Registration Successful. You can now log in.');
+    wrapper.classList.add('active-popup');
+}
